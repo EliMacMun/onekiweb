539c5d29b39054584d4e205cea33187989f047bc
Elí Maciel <tecno_eli@hotmail.com>
2021-07-04 10:07:58 -0500

    navbar agregado

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 //settings
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
 
 //midlewares
 app.use(morgan("dev"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,13 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', async (req, res, next) => {
-    res.send('En construccion, disculpe las molestias')
+    res.render('index', {
+        title: 'Oneki Bot',
+        bot: {
+            username: 'Oneki',
+            avatar: 'https://cdn.discordapp.com/avatars/858903483004354560/4137e61af297c696d98321f56bc8abc2.png?size=64'
+        }
+    });
 });
 
 module.exports = router;
