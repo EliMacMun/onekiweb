46408cf73f9db9d3125a5319f0fa3772aa224557
Elí Maciel <tecno_eli@hotmail.com>
2021-09-07 21:53:23 -0500

    `/api/lang`  corregido

--- src/routes/api/lang.js
+++ src/routes/api/lang.js
@@ -13,6 +13,26 @@ router.get('/:lang/cmd/:category', (req, res) => {
     }
     if (category==='categories') res.json(cmd.map(c=>c.category).filter((e, i, a)=>a.indexOf(e)===i));
     else if (category==='all') res.json(cmd);
-    else if (!cmd.filter(c=>c.category===category)) res.sendStatus(500).send('Category not found')
+    else if (!cmd.filter(c=>c.category===category)) res.status(500).send({
+        error: 'Category not found'
+    })
     else res.json(cmd.filter(c=>c.category===category));
-})
+});
+
+router.get('/:lang/cmd/', (req, res) => {
+    const { lang } = req.params;
+    const { command } = req.query;
+    let cmd;
+    try {
+        cmd = require(`../lang/${lang}/cmd.json`);
+    } catch (error) {
+        cmd = require(`../lang/en/cmd.json`);
+    }
+    if (command) {
+        // noinspection JSValidateTypes
+        if (!cmd.find(c=>c.name===command||c.alias.includes(command))) res.status(500).send({
+            error: 'Category not found'
+        });
+        else res.json(cmd.find(c=>c.name===command||c.alias.includes(command)))
+    } else res.json(cmd);
+});
