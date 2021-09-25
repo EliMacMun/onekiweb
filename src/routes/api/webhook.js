0901f667bb2343ae43a4610b13356d319d9104fc
Elí Maciel <tecno_eli@hotmail.com>
2021-09-25 18:01:01 -0500

    Update webhook.js

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -4,14 +4,14 @@ const router = Router();
 
 router.post('/heroku', (req, res) => {
     console.log(req.body)
-            fetch('https://canary.discord.com/api/webhooks/883159152183091202/dIMlDJXworBoB6SukT0vngRYrQC8v0iDgLjBStXoBSPoTd9EfqQM9SBGVevaSm3ke0uP', {
-                method: 'POST',
-                body: JSON.stringify({
-                    content: `\`\`\`json\n${JSON.stringify(req.body, null, 4)}\n\`\`\``,
-                    username: 'Heroku',
-                    avatar_url: "https://brandeps.com/logo-download/H/Heroku-logo-vector-01.svg"
-                })
-            })
+    fetch('https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS', {
+        method: 'POST',
+        body: JSON.stringify({
+            content: `\`\`\`json\n${JSON.stringify(req.body, null, 4)}\n\`\`\``,
+            username: 'Heroku',
+            avatar_url: "https://brandeps.com/logo-download/H/Heroku-logo-vector-01.svg"
+        })
+    })
     res.json({
         status: "200 ok"
     })
