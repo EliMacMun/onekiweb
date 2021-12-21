57ab496925d117ac74f90c1d1fb89be13f728a4e
Elí Maciel <tecno_eli@hotmail.com>
2021-12-20 22:15:10 -0600

    Update webhook.js

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -1,11 +1,20 @@
 'use strict';
 const { Router } = require('express');
-const {WebhookClient} = require("discord.js");
+const {WebhookClient, MessageEmbed} = require("discord.js");
 const router = Router();
 
 router.post('/heroku', (req, res) => {
+    const {action, actor, created_at, id, data:{release, app, state, command}} = req.body
     (new WebhookClient({ url: 'https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS'})).send({
-        content: `\`\`\`json\n${JSON.stringify(req.body, null, 4)}\n\`\`\``
+        embeds: [
+            new MessageEmbed()
+                .setURL(`https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`)
+                .setTitle(`${action} ${app.name} [v${release.version}]`)
+                .setAuthor(actor.email, 'https://icon-library.com/icon/heroku-icon-24.html.html', `https://dashboard.heroku.com/apps/${app.name}`)
+                .addField('Time', `<t:${new Date(created_at).getTime()/100}>`)
+                .addField('State', `\`\`\`\n${state}\n\`\`\``)
+                .addField('Execute', `\`\`\`\n${command}\n\`\`\``)
+        ]
     })
     res.json({
         status: "200 ok"
