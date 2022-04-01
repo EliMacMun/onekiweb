57b0e44281b45e515a93b6c9eac7a8643ed01ed2
Elí Maciel <tecno_eli@hotmail.com>
2022-03-31 19:09:49 -0600

    no comment

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -1,20 +1,22 @@
 import { Router } from 'express'
-import { WebhookClient, MessageEmbed } from 'discord.js'
 const router = Router();
 
 router.post('/heroku', (req, res) => {
     const {action, actor, created_at, id, data:{release, app, state, command}} = req.body
-    (new WebhookClient({ url: 'https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS'})).send({
-        embeds: [
-            new MessageEmbed()
-                .setURL(`https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`)
-                .setTitle(`${action} ${app.name} [v${release.version}]`)
-                .setAuthor(actor.email, 'https://icon-library.com/icon/heroku-icon-24.html.html', `https://dashboard.heroku.com/apps/${app.name}`)
-                .addField('Time', `<t:${new Date(created_at).getTime()/100}>`)
-                .addField('State', `\`\`\`\n${state}\n\`\`\``)
-                .addField('Execute', `\`\`\`\n${command}\n\`\`\``)
-        ]
+    fetch('https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS', {
+
     })
+    // (new WebhookClient({ url: ''})).send({
+    //     embeds: [
+    //         new MessageEmbed()
+    //             .setURL(`https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`)
+    //             .setTitle(`${action} ${app.name} [v${release.version}]`)
+    //             .setAuthor(actor.email, 'https://icon-library.com/icon/heroku-icon-24.html.html', `https://dashboard.heroku.com/apps/${app.name}`)
+    //             .addField('Time', `<t:${new Date(created_at).getTime()/100}>`)
+    //             .addField('State', `\`\`\`\n${state}\n\`\`\``)
+    //             .addField('Execute', `\`\`\`\n${command}\n\`\`\``)
+    //     ]
+    // })
     res.json({
         status: "200 ok"
     })
