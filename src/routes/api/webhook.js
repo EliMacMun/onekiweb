a117f238bbf63498002cb5de7759985dfe3f6cfd
Elí Maciel <tecno_eli@hotmail.com>
2022-03-31 19:16:24 -0600

    hebhook refactorized

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -1,25 +1,59 @@
 import { Router } from 'express'
-const router = Router();
+const router = Router()
 
 router.post('/heroku', (req, res) => {
-    const {action, actor, created_at, id, data:{release, app, state, command}} = req.body
-    fetch('https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS', {
+    const {
+        action,
+        actor,
+        created_at,
+        id,
+        data: { release, app, state, command }
+    } = req.body
+    fetch(
+        'https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: 'Heroku',
+                avatar_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
+                embeds: [
+                    {
+                        title: `${action} ${app.name} [v${release.version}]`,
+                        url: `https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`,
+                        author: {
+                            name: actor.email,
+                            icon_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
+                            url: `https://dashboard.heroku.com/apps/${app.name}`
+                        },
+                        fields: [
+                            {
+                                name: 'Time',
+                                value: `<t:${new Date(created_at).getTime() / 100}>`,
+                                inline: true
+                            },
+                            {
+                                name: 'State',
+                                value: `\`\`\`\n${state}\n\`\`\``,
+                                inline: true
+                            },
+                            {
+                                name: 'Execute',
+                                value: `\`\`\`\n${command}\n\`\`\``,
+                                inline: true
+                            }
+                        ]
+                    }
+                ]
+            })
+        }
+    )
 
-    })
-    // (new WebhookClient({ url: ''})).send({
-    //     embeds: [
-    //         new MessageEmbed()
-    //             .setURL(`https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`)
-    //             .setTitle(`${action} ${app.name} [v${release.version}]`)
-    //             .setAuthor(actor.email, 'https://icon-library.com/icon/heroku-icon-24.html.html', `https://dashboard.heroku.com/apps/${app.name}`)
-    //             .addField('Time', `<t:${new Date(created_at).getTime()/100}>`)
-    //             .addField('State', `\`\`\`\n${state}\n\`\`\``)
-    //             .addField('Execute', `\`\`\`\n${command}\n\`\`\``)
-    //     ]
-    // })
     res.json({
-        status: "200 ok"
+        status: '200 ok'
     })
-});
+})
 
 export { router as webhook }
