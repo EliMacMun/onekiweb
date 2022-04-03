2c2aea2c766abc8882e3405883fa0c3c663ad8ef
Elí Maciel <tecno_eli@hotmail.com>
2022-04-03 16:55:06 -0500

    added functionality to invite button

--- src/routes/web.js
+++ src/routes/web.js
@@ -20,17 +20,17 @@ router.get('/login', passport.authenticate('discord', { failureRedirect: '/' }),
     res.redirect(req.flash('redi')[0] ?? '/')
 })
 
-// router.get('/invite', (req, res) => {
-//     res.redirect('https://discord.com/api/oauth2/authorize?client_id=901956486064922624&permissions=8&redirect_uri=https%3A%2F%2Foneki.herokuapp.com%2Flogin&response_type=code&scope=bot%20identify%20guilds%20applications.commands');
-// });
+router.get('/invite', (req, res) => {
+    res.redirect('https://discord.com/api/oauth2/authorize?client_id=901956486064922624&permissions=8&redirect_uri=https%3A%2F%2Foneki.herokuapp.com%2Flogin&response_type=code&scope=bot%20identify%20guilds%20applications.commands');
+})
 
 // router.get('/thanks', (req, res) => {
 //     if(!req.user) res.redirect('/login')
 //     else res.redirect('/')
 // })
 
-// router.get('/discord', (req, res) => {
-//     res.redirect('https://discord.gg/8SpUxnF6v4')
-// })
+router.get('/discord', (req, res) => {
+    res.redirect('https://discord.gg/8SpUxnF6v4')
+})
 
 export { router as web }
