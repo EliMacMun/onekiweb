b89c253f9dc786c040b7250f561aaa9441b166ca
Elí Maciel <tecno_eli@hotmail.com>
2022-04-03 15:58:02 -0500

    webhook test

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -9,6 +9,7 @@ router.post('/heroku', (req, res) => {
         id,
         data: { release, app, state, command }
     } = req.body
+    console.log(req.body)
     fetch(
         'https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS',
         {
@@ -17,39 +18,51 @@ router.post('/heroku', (req, res) => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                username: 'Heroku',
-                avatar_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
-                embeds: [
-                    {
-                        title: `${action} ${app.name} [v${release.version}]`,
-                        url: `https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`,
-                        author: {
-                            name: actor.email,
-                            icon_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
-                            url: `https://dashboard.heroku.com/apps/${app.name}`
-                        },
-                        fields: [
-                            {
-                                name: 'Time',
-                                value: `<t:${new Date(created_at).getTime() / 100}>`,
-                                inline: true
-                            },
-                            {
-                                name: 'State',
-                                value: `\`\`\`\n${state}\n\`\`\``,
-                                inline: true
-                            },
-                            {
-                                name: 'Execute',
-                                value: `\`\`\`\n${command}\n\`\`\``,
-                                inline: true
-                            }
-                        ]
-                    }
-                ]
+                content: '```'+JSON.stringify(req.body, null, 2)+'```'
             })
         }
     )
+    // fetch(
+    //     'https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS',
+    //     {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             username: 'Heroku',
+    //             avatar_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
+    //             embeds: [
+    //                 {
+    //                     title: `${action} ${app.name} [v${release.version}]`,
+    //                     url: `https://dashboard.heroku.com/apps/${app.name}/activity/builds/${id}`,
+    //                     author: {
+    //                         name: actor.email,
+    //                         icon_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
+    //                         url: `https://dashboard.heroku.com/apps/${app.name}`
+    //                     },
+    //                     fields: [
+    //                         {
+    //                             name: 'Time',
+    //                             value: `<t:${new Date(created_at).getTime() / 100}>`,
+    //                             inline: true
+    //                         },
+    //                         {
+    //                             name: 'State',
+    //                             value: `\`\`\`\n${state}\n\`\`\``,
+    //                             inline: true
+    //                         },
+    //                         {
+    //                             name: 'Execute',
+    //                             value: `\`\`\`\n${command}\n\`\`\``,
+    //                             inline: true
+    //                         }
+    //                     ]
+    //                 }
+    //             ]
+    //         })
+    //     }
+    // )
 
     res.json({
         status: '200 ok'
