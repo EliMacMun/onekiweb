655f810851380e1f5e95e02542ecd17162c754c8
Elí Maciel <tecno_eli@hotmail.com>
2022-04-03 16:44:34 -0500

    /heroku/release added

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 const router = Router()
 
-router.post('/heroku', (req, res) => {
-    const { action, actor, created_at, id, data, version } = req.body
+router.post('/heroku/release', (req, res) => {
+    const { webhook_metadata, action, actor, created_at, id, data, version } = req.body
     fetch(
         'https://canary.discord.com/api/webhooks/891458927479377930/ZiTKdJkpIuyd1x3E3IDVMn1ecd7aHGsd-lPb89P-KDJ5RoafxLGUKc6ReptDB3iCuRhS',
         {
@@ -15,30 +15,41 @@ router.post('/heroku', (req, res) => {
                 avatar_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
                 embeds: [
                     {
-                        title: `${action} ${app.name} [v${version}]`,
-                        url: `https://dashboard.heroku.com/apps/${data.name}/activity/builds/${id}`,
+                        title: `${webhook_metadata.event.include} ${action} ${data.app.name}`,
+                        url: `https://dashboard.heroku.com/apps/${data.app.name}/activity/builds/${id}`,
                         author: {
                             name: actor.email,
                             icon_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg',
-                            url: `https://dashboard.heroku.com/apps/${data.name}`
+                            url: `https://dashboard.heroku.com/apps/${data.app.name}`
                         },
+                        description: data.description,
                         fields: [
                             {
-                                name: 'Time',
-                                value: `<t:${new Date(created_at).getTime() / 100}>`,
+                                name: 'Execute',
+                                value: `\`\`\`\n${data.pstable.web.command}\n\`\`\``,
                                 inline: true
                             },
-                            // {
-                            //     name: 'State',
-                            //     value: `\`\`\`\n${state}\n\`\`\``,
-                            //     inline: true
-                            // },
-                            // {
-                            //     name: 'Execute',
-                            //     value: `\`\`\`\n${command}\n\`\`\``,
-                            //     inline: true
-                            // }
-                        ]
+                            {
+                                name: 'Commit',
+                                value: `\`\`\`\n[${data.slug.commit}]\n${data.slug.commit_description}\n\`\`\``,
+                                inline: true
+                            },
+                            {
+                                name: 'Version',
+                                value: `\`\`\`\n${version}\n\`\`\``,
+                                inline: true
+                            },
+                            {
+                                name: 'Status',
+                                value: `\`\`\`\n${data.status}\n\`\`\``,
+                                inline: true
+                            }
+                        ],
+                        timestamp: new Date(created_at),
+                        footer: {
+                            text: `${actor.email}`,
+                            icon_url: 'https://icon-library.com/images/heroku-icon/heroku-icon-24.jpg'
+                        }
                     }
                 ]
             })
