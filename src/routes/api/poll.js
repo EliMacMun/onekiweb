f183f254b38c08f941be3b040962d05213f52572
tecno_eli@hotmail.com <eliyya>
2022-05-10 02:01:50 -0500

    add poll view

--- src/routes/api/poll.js
+++ src/routes/api/poll.js
@@ -1,4 +1,10 @@
 import { Router } from 'express'
 const router = Router();
 
+router.get('/:id', (req, res) => {
+    res.render('poll', {
+        user: req.user
+    })
+})
+
 export { router as poll }
