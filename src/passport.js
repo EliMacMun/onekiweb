fcfbcca007487de41304185830525980db74e5de
tecno_eli@hotmail.com <eliyya>
2022-05-09 23:24:14 -0500

    reset secret

--- src/passport.js
+++ src/passport.js
@@ -1,30 +1,41 @@
 import passport from 'passport'
-import { Strategy } from 'passport-discord'
+import Strategy from 'passport-discord'
 import { config } from 'dotenv'
 config()
 
 passport.serializeUser((user, done) => {
-    done(null, user)
-})
+    done(null, user);
+});
 
 passport.deserializeUser((obj, done) => {
-    done(null, obj)
+    done(null, obj);
+});
+
+console.log({
+    clientID: BigInt(process.env.CLIENT_ID),
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.REDIRECT_URL   
 })
 
-passport.use(
-    new Strategy(
-        {
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: process.env.REDIRECT_URL,
-            scope: ['identify']
-        },
-        (accesToken, refreshToken, profile, cb) => {
-            process.nextTick(() => {
-                return cb(null, profile)
-            })
-        }
-    )
-)
+passport.use(new Strategy({
+    clientID: BigInt(process.env.CLIENT_ID, 10),
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.REDIRECT_URL,
+    scope: ['identify', 'email', 'guilds'],
+    // authorizationURL: 'https://discordapp.com/api/oauth2/authorize',
+    // clientID: process.env.CLIENT_ID,
+    // clientSecret: process.env.CLIENT_SECRET,    
+    // callbackURL: process.env.REDIRECT_URL,
+    // scope: ["identify"]
+}, (accesToken, refreshToken, profile, cb) => {
+    console.log({
+        accesToken,
+        refreshToken,
+        profile
+    })
+    process.nextTick(() => {
+        return cb(null, profile)
+    });
+}));
 
-export { passport }
+export default passport
