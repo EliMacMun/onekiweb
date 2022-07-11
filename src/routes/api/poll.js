256d85f11a82ef48f4bb0261a7ff3aa50492192e
tecno_eli@hotmail.com <eliyya>
2022-07-10 23:24:25 -0500

    fix:bug

--- src/routes/api/poll.js
+++ src/routes/api/poll.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 const router = Router()
 import db from '../db.js'
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const snap = await db.collection('polls').doc(req.params.id).get()
     if (!snap.exists) res.status(404).send('Not found')
     else res.status(200).json(snap.data())
