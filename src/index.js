0e1ad96d78c41a78858624519a82f0355759b6fb
Elí Maciel <tecno_eli@hotmail.com>
2022-08-08 03:43:32 -0500

    yt notifier configurated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 // @ts-check
 import notifier from './youtube.js'
 import serverApp from './app.js'
-
-// youtube
-notifier.subscribe('UCiVty0vnYbswLGhmWTp6FPA')
+import db from './db.js'
 
 //routes
-import('./routes/web.js').then(web => {
-    serverApp.app.use('/', web.default)
-})
+import('./routes/web.js').then(web => serverApp.app.use('/', web.default))
 
-import('./routes/api.js').then(api => {
-    serverApp.app.use('/api', api.default)
-})
+import('./routes/api.js').then(api => serverApp.app.use('/api', api.default))
+
+// youtube
+const snap = await db.collection('notifications').doc('youtube').get()
+if (snap.exists) for (const key in snap.data()) notifier.subscribe(key)
