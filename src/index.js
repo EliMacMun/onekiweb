3eaea52ebca9474b6c99d008d6b7efefc425da0e
Elí Maciel <tecno_eli@hotmail.com>
2022-08-11 13:17:41 -0500

    check

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// @ts-check
 import notifier from './youtube.js'
 import serverApp from './app.js'
 import db from './db.js'
+
+// youtube
+const snap = await db.collection('notifications').doc('youtube').get()
+if (snap.exists) notifier.subscribe(Object.keys(snap.data()))
 
 //routes
 import('./routes/web.js').then(web => serverApp.app.use('/', web.default))
 
 import('./routes/api.js').then(api => serverApp.app.use('/api', api.default))
-
-// youtube
-const snap = await db.collection('notifications').doc('youtube').get()
-if (snap.exists) for (const key in snap.data()) notifier.subscribe(key)
--- src/youtube.js
+++ src/youtube.js
@@ -1,24 +1,45 @@
-// @ts-check
 import YouTubeNotifier from 'youtube-notification'
 import db from './db.js'
 import socket from './socket.js'
 
-export default new YouTubeNotifier({
-    hubCallback: 'https://oneki.up.railway.app/api/notifications/youtube/',
-    secret: 'IDK'
-}).on('notified', async data => {
-    const snap = await db.collection('notifications').doc('youtube').get()
-    if (!snap.exists) return
+export default new YouTubeNotifier(
+    {
+        hubCallback: 'http://localhost:3000/api/notifications/youtube',
+        // hubCallback: 'https://oneki.up.railway.app/api/notifications/youtube/',
+        secret: 'otherSecret'
+    },
+    (console.log('x'), true)
+)
+    .on('notified', async data => {
+        console.log(data)
 
-    snap.data()?.[data.channel.id]?.map(id => {
-        socket.clients.forEach(client => {
-            client.send(
-                JSON.stringify({
-                    event: 'new_yt_video',
-                    server: id,
-                    value: data
-                })
-            )
+        const snap = await db.collection('notifications').doc('youtube').get()
+        if (!snap.exists) return
+
+        snap.data()?.[data.channel.id]?.map(id => {
+            socket.clients.forEach(client => {
+                client.send(
+                    JSON.stringify({
+                        event: 'new_yt_video',
+                        from: 'mweb',
+                        data: {
+                            server: id,
+                            data
+                        }
+                    })
+                )
+            })
         })
     })
-})
+    .on('subscribe', data => {
+        console.log('Subscribed')
+        console.log(data)
+    })
+    .on('unsubscribe', data => {
+        console.log('Unsubscribed')
+        console.log(data)
+    })
+    .on('denied', data => {
+        console.log('Denied')
+        console.log(data)
+    })
